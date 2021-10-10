9ca27a335b503489b71f200bf55eae5b95745705
Jaypal Koli <69342922+Jaypal07@users.noreply.github.com>
2021-10-10 07:50:41 +0530

    Update SwapNumber.java

--- SwapNumber.java
+++ SwapNumber.java
@@ -2,18 +2,15 @@ public class SwapNumber {
 
     public static void main(String[] args) {
 
-        float first = 1.20f, second = 2.45f;
+       float first = 12.0f, second = 24.5f;
 
         System.out.println("--Before swap--");
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
 
-        float temporary = first;
-
-        first = second;
-
-        second = temporary;
-
+        first = first - second;
+        second = first + second;
+        first = second - first;
         System.out.println("--After swap--");
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
