72fa882ee6e97db58b58b8ab03dcaed612181110
Jay Wagh <91671311+Jaywagh@users.noreply.github.com>
2021-10-10 07:45:22 +0530

    Update ReverseNum.java
    use for loop

--- ReverseNum.java
+++ ReverseNum.java
@@ -1,18 +1,12 @@
 class ReverseNum {
     public static void main(String[] args) {
 
-        int num = 1234, reversed = 0;
+        int num = 1234567, reversed = 0;
 
-        // run loop until num becomes 0
-        while (num != 0) {
-
-            // get last digit from num
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-
-            // remove the last digit from num
-            num /= 10;
-        }
+    for(;num != 0; num /= 10) {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+    }
 
         System.out.println("Reversed Number: " + reversed);
     }
